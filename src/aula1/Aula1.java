d904e8cd3ad1c57c1629d02b1c5692b1a614d847
Ricardo <anjos.ricardo@gmail.com>
2020-07-21 19:28:24 -0300

    Aula 3 - Demostração de estrutura de controle "if". Operador lógico (or).

--- src/aula1/Aula1.java
+++ src/aula1/Aula1.java
@@ -5,7 +5,7 @@ public class Aula1 {
 	public static void main(String[] args) {
 		
 		Carro carro = new Carro();
-		carro.freiar();
+		carro.buzinar();
 		System.out.println("Qual a sua cor: ");
 		System.out.println(carro.cor);
 		
